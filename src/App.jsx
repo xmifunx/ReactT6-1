9339b9d5e4025b9dd57f65edc35217d41496c20c
xmifunx <r.eustafjev@yandex.ru>
2024-10-06 18:30:22 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import Clock from './components/Clock/Clock.jsx';
+import Form from './components/Form/Form.jsx';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    const [clocks, setClocks] = useState([]);
+
+    function handleFormSubmit(form) {
+        setClocks((prevState) => [...prevState, {
+            id: nanoid(),
+            name: form.name,
+            userTimezone: form.userTimezone,
+        }]);
+    }
+
+    function getClockIndex(id) {
+        const index = clocks.findIndex((clock) => clock.id === id);
+
+        return index;
+    }
+
+    function handleDeleteClick(id) {
+        const index = getClockIndex(id);
+
+        const updatedClocks = [
+            ...clocks.slice(0, index),
+            ...clocks.slice(index + 1),
+        ];
+
+        setClocks(updatedClocks);
+    }
+
+    return (
+        <div className="App-container">
+            <Form onFormSubmit={handleFormSubmit} />
+            <div className="App-clocks-container">
+                {clocks.map((clock) => {
+                    return (
+                        <Clock
+                            key={clock.id}
+                            id={clock.id}
+                            name={clock.name}
+                            userTimezone={clock.userTimezone}
+                            onDeleteClick={handleDeleteClick}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import '../../App.css';
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.nameRef = React.createRef();
+        this.state = { name: '', userTimezone: '' };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    componentDidMount() {
+        this.nameRef.current.focus();
+    }
+
+    handleInputChange({ target }) {
+        const { name, value } = target;
+
+        this.setState(prevForm => ({ ...prevForm, [name]: value }));
+    }
+
+    render() {
+        return (
+            <form
+                className="Form"
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    this.props.onFormSubmit(this.state);
+                    this.setState({ name: '', userTimezone: '' });
+                    this.nameRef.current.focus();
+                }}
+            >
+                <div className="Form-control">
+                    <label htmlFor="name">Название</label>
+                    <input
+                        className="Form-control__name"
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleInputChange}
+                        ref={this.nameRef}
+                        autoComplete="off"
+                        required
+                    />
+                </div>
+                <div className="Form-control">
+                    <label htmlFor="user-timezone">Временная зона</label>
+                    <input
+                        className="Form-control__user-timezone"
+                        type="number"
+                        id="user-timezone"
+                        name="userTimezone"
+                        min="-12"
+                        max="14"
+                        value={this.state.userTimezone}
+                        onChange={this.handleInputChange}
+                        required
+                    />
+                </div>
+                <button
+                    className="Form-control__button-add"
+                    type="submit"
+                >
+                    Добавить
+                </button>
+            </form>
+        );
+    }
+}
+
+export default Form;
